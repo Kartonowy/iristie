d7ebb48d8ed6ce34cb25f2c7120f7f7ad3f14e9d
Kartonowy <ttrollface.com.pl@gmail.com>
2026-02-11 10:23:29 +0100

    scaffolding

--- src/modules/pot/Pot.vue
+++ src/modules/pot/Pot.vue
@@ -1,13 +1,39 @@
 <template>
     <div class="styler">
         <div class="top">
-            <div class="players"></div>
+            <div class="players">
+                <div class="player"></div>
+                <div class="player"></div>
+                <div class="player"></div>
+                <div class="player"></div>
+            </div>
             <img class="pot"
                 src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.zerochan.net%2FPot.of.Greed.full.3789420.png"
                 alt="Pot">
-            <div class="options"></div>
+            <div class="options">
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+                <div class="option"></div>
+            </div>
         </div>
-        <div class="cards"></div>
+        <div class="cards">
+            <div class="card A"></div>
+            <div class="card B"></div>
+            <div class="card C"></div>
+        </div>
     </div>
 </template>
 
@@ -24,20 +50,46 @@
     flex-flow: row nowrap;
     justify-content: space-around;
     width: 100vw;
-    margin: 2vmin;
+    margin-top: 1%;
 }
 
 .cards {
+    margin-top: 3vh;
     width: 60vw;
-    height: 50vh;
+    height: 45vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+}
+
+.card {
+    width: 25%;
+    height: 85%;
 }
 
 .players {
     width: 20vw;
+    height: 40vh;
+    overflow: hidden;
+    overflow-y: scroll;
+}
+
+.player {
+    width: 100%;
+    height: 4vh;
 }
 
 .options {
     width: 20vw;
+    height: 40vh;
+    display: grid;
+    grid-template-columns: auto auto auto auto;
+    grid-template-rows: 25% 25% 25% 25%;
+}
+
+.option {
+    width: 100%;
+    height: 100%;
 }
 
 .pot {
@@ -46,7 +98,10 @@
 
 .cards,
 .players,
-.options {
-    border: 1px solid red;
+.options,
+.option,
+.card,
+.player {
+    outline: 1px solid red;
 }
 </style>
