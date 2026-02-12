a869b00f9d405780f668f10e3738d8af92de4149
Kartonowy <ttrollface.com.pl@gmail.com>
2026-02-12 12:47:31 +0100

    removed unused

---
+++ src/modules/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="sect">
+        <h2>what is this ??</h2>
+        <p>
+        </p>
+    </div>
+    <div class="sect">
+        <h2>Why shikanoko.jelenia-gora.pl?</h2>
+        <p>
+            Shikanoko, or しかのこ (鹿の子), means <i>"deer child"</i> in Japanese. It's also a name of the main character from
+            <i>Shikanoko nokonoko koshitantan</i> anime, which is a pretty brainrotty one. "<i>jelenia-gora</i>" means 
+            "<i>deer mountain</i>" in Polish, thus also the pl at the end. It's just a silly reference to the anime and the main domain.
+        </p>
+    </div>
+</template>
