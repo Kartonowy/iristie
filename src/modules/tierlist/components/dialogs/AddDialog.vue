e52ad1367a6670b39ccb9402588a1d039612e27b
Kartonowy <ttrollface.com.pl@gmail.com>
2026-04-09 12:12:02 +0200

    tiers updategit add src/modules/tierlist!

--- src/modules/tierlist/components/dialogs/AddDialog.vue
+++ src/modules/tierlist/components/dialogs/AddDialog.vue
@@ -21,8 +21,15 @@ const series = ref("")
 const short = ref("")
 const image = ref("")
 const tier = ref("SSS")
+const unsetfunc = ref(() => {})
 
-
+const setTier = (e: any) => {
+    const t = e.target?.className
+    unsetfunc.value()
+    tier.value = t
+    e.target?.classList.add('selected')
+    unsetfunc.value = () => {e.target?.classList.remove('selected')}
+}
 
 
 const handleAdd = () => {
@@ -61,18 +68,17 @@ const handleAdd = () => {
     <input type="text" name="series" placeholder="series" v-model="series">
     <input type="text" name="src" placeholder="image url" v-model="image">
     <label>
-        tier:
-        <select name="tier" v-model="tier">
-            <option value="SSS">SSS</option>
-            <option value="SS">SS</option>
-            <option value="S">S</option>
-            <option value="A">A</option>
-            <option value="B">B</option>
-            <option value="C">C</option>
-            <option value="D">D</option>
-            <option value="E">E</option>
-            <option value="F">F</option>
-        </select>
+        <span class="tiers">
+            <span class="SSS" @click="setTier">SSS</span>
+            <span class="SS" @click="setTier">SS</span>
+            <span class="S" @click="setTier">S</span>
+            <span class="A" @click="setTier">A</span>
+            <span class="B" @click="setTier">B</span>
+            <span class="C" @click="setTier">C</span>
+            <span class="D" @click="setTier">D</span>
+            <span class="E" @click="setTier">E</span>
+            <span class="F" @click="setTier">F</span>
+        </span>
     </label>
     <textarea name="short" placeholder="Short reasoning" rows="20" cols="30" v-model="short" />
     <input type="button" value="Submit" @click=handleAdd>
@@ -82,4 +88,64 @@ const handleAdd = () => {
     .auth {
         width: 25%
     }
+    .tiers {
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+    .selected {
+        outline: 3px wheat solid;
+    }
+
+    .tiers span {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-top: 1vh;
+        margin-bottom: 1vh;
+        margin-left: 0.03vw;
+        margin-right: 0.03vw;
+        color: #423939;
+        padding-top: 2px;
+        padding-bottom: 2px;
+        width: 33px;
+        height: 33px;
+        border-radius: 3px;
+    }
+.SSS {
+    background: linear-gradient(-66.6deg, #ff7f7f 0%, red 100%);
+}
+
+.SS {
+    background-color: #ff7f7f;
+}
+
+.S {
+    background-color: #ffbf7f;
+}
+
+.A {
+    background-color: #ffdf7f;
+}
+
+.B {
+    background-color: #ffff7f;
+}
+
+.C {
+    background-color: #bfff7f;
+}
+
+.D {
+    background-color: #7fff7f;
+}
+
+.E {
+    background-color: #7fffff;
+}
+
+.F {
+    background-color: #7fbfff;
+}
 </style>
